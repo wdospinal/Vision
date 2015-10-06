b965756790681a0abe51813f4eb59990bea1b68c
Ivan Portilla <jicuastumalp@unal.edu.co>
2015-10-06 12:18:32 -0500

    Filter average fixed

--- filter_test.m
+++ filter_test.m
@@ -1,7 +1,10 @@
 handler = functions_helper();
 image = imread('test.jpg');
 [R G B] = handler.get_rgb_channels(image);
-I = handler.histogram_expansion(R);
-I2 = imadjust(R);
+% I = handler.histogram_expansion(im2double(R));
+% I2 = imadjust(im2double(R));
+% 
+% imhist(I); figure, imhist(I2);
 
-imhist(I); figure, imhist(I2);
+%I = handler.average_filter(R, 3, 3);
+%imshow(I);
